4da46410752ded5b6a19ecbb423d622ff607f53a
Max Calvert-Minor <maxincalvertminor@gmail.com>
2025-02-27 17:21:45 -0600

    NodeGrid & Node name changes

--- Assets/Node.cs
+++ Assets/Node.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NavTile
+public class Node
 {
     public Vector2 position;
     public float g_Cost;
     public float h_Cost;
     public bool is_available;
 
-    public NavTile(Vector2 pos) {
+    public Node(Vector2 pos) {
         position = pos;
     }
     public float f_Cost() {
--- Assets/NodeGrid.cs
+++ Assets/NodeGrid.cs
@@ -2,25 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NavGrid : MonoBehaviour
+public class NodeGrid : MonoBehaviour
 {
 
     public RectTransform rect_transform;
     public int grid_x;
     public int grid_y;
-    private NavTile[,] nav_grid;
-    public NavTile start;
-    public NavTile goal;
+    private Node[,] node_grid;
+    public Node start;
+    public Node goal;
     public float radius;
 
     // Start is called before the first frame update
     void Start()
     {
         rect_transform = GetComponent<RectTransform>();
-        nav_grid = new NavTile[grid_y, grid_x];
+        node_grid = new Node[grid_y, grid_x];
         for(int r = 0; r < grid_y; r++) {
             for(int c = 0; c < grid_x; c++) {
-                nav_grid[r, c] = new NavTile(new Vector2(radius * (2 * c + 1), radius * (2 * r + 1)));
+                node_grid[r, c] = new Node(new Vector2(radius * (2 * c + 1), radius * (2 * r + 1)));
             }
         }
     }
